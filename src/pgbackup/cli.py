e41b37966d3efe3d03d305b4119bbe752770f14d
Jacob Archambault <jacobarchambault@gmail.com>
2024-04-29 17:21:16 -0400

    Improve modularity

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,26 +1,10 @@
-from argparse import ArgumentParser
-from pgbackup import driver_action
-
-def create_parser():
-    parser = ArgumentParser(description=""" 
-    Back up PostgreSQL databases locally or to AWS S3
-    """)
-    parser.add_argument("url", help="URL of the database")
-    parser.add_argument("--driver", 
-    '-d', 
-    help="How and where to store backup", 
-    nargs=2, 
-    metavar=("DRIVER", "DESTINATION"),
-    action=DriverAction, 
-    required=True)
-    return parser
 
 def main():
     import boto3
     import time
-    from pgbackup import pgdump, storage
+    from pgbackup import pgdump, storage, parser
 
-    args = create_parser().parse_args()
+    args = parser.create_parser().parse_args()
     dump = pgdump.dump(args.url)
     if args.driver == 's3':
         client = boto3.client('s3')
