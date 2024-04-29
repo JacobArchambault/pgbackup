096b44d0b6995ec3189ba382099b9d7e17d02b8f
Jacob Archambault <jacobarchambault@gmail.com>
2024-04-29 14:21:21 -0400

    MVP before fixing setuptools issue

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -10,5 +10,27 @@ def create_parser():
     Back up PostgreSQL databases locally or to AWS S3
     """)
     parser.add_argument("url", help="URL of the database")
-    parser.add_argument("--driver", help="How and where to store backup", nargs=2, action=DriverAction, required=True)
+    parser.add_argument("--driver", 
+    '-d', 
+    help="How and where to store backup", 
+    nargs=2, 
+    metavar=("DRIVER", "DESTINATION"),
+    action=DriverAction, 
+    required=True)
     return parser
+
+def main():
+    import boto3
+    import time
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        storage.local(dump.stdout, outfile)
