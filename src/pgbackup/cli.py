1be7b3bf697c9c8c69af70af936f3573180d9597
Jacob Archambault <jacobarchambault@gmail.com>
2024-04-30 19:05:45 -0400

    Inline pgdump.dump call

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -4,13 +4,12 @@ from pgbackup import pgdump, storage, parser
 def main():
 
     args = parser.create_parser().parse_args()
-    dump = pgdump.dump(args.url)
     if args.driver == 's3':
         storage.s3(
-            dump, 
+            pgdump.dump(args.url), 
             args.destination, 
             pgdump.dump_file_name(args.url))
     else:
         storage.local(
-            dump, 
+            pgdump.dump(args.url), 
             open(args.destination, 'wb'))
