91bd3b70a74d79c2fbcde80f5ac345c03ec1e125
Jacob Archambault <jacobarchambault@gmail.com>
2024-05-15 10:45:26 -0400

    wrap arguments in s3_storage.backup function

--- src/pgbackup/s3_storage.py
+++ src/pgbackup/s3_storage.py
@@ -2,4 +2,7 @@ import boto3
 from pgbackup import pgdump, timestamped_file_name
 
 def backup(url, bucket):
-    boto3.client('s3').upload_fileobj(pgdump.dump(url), bucket, timestamped_file_name.get(url))
+    boto3.client('s3').upload_fileobj(
+        pgdump.dump(url), 
+        bucket, 
+        timestamped_file_name.get(url))
