b35835c4238bf1bc4a92d5f1fe1fdd5d6daff02a
Jacob Archambault <jacobarchambault@gmail.com>
2024-05-15 13:49:17 -0400

    Have driver err if bad driver is provided

--- src/pgbackup/driver_action.py
+++ src/pgbackup/driver_action.py
@@ -3,5 +3,7 @@ from argparse import Action
 class DriverAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         driver, destination=values
+        if driver.lower() not in ['local', 's3']:
+            parser.error("Unknown driver. Available drivers are 'local' & 's3'")
         namespace.driver = driver.lower()
         namespace.destination = destination
