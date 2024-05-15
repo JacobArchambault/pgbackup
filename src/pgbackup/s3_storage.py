bb66ce72f9311dc585a8a1df218d31983271804e
Jacob Archambault <jacobarchambault@gmail.com>
2024-05-15 10:53:14 -0400

    Rename timestamped_file_name to timestamped_db_name.py

--- src/pgbackup/s3_storage.py
+++ src/pgbackup/s3_storage.py
@@ -1,8 +1,8 @@
 import boto3
-from pgbackup import pgdump, timestamped_file_name
+from pgbackup import pgdump, timestamped_db_name
 
 def backup(url, bucket):
     boto3.client('s3').upload_fileobj(
         pgdump.dump(url), 
         bucket, 
-        timestamped_file_name.get(url))
+        timestamped_db_name.get(url))
